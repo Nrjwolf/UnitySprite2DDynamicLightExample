df2ea7fa0464c7438a7af85beeb26263c348612c
Alexander <Nrjwolf@MacBook-Pro-Alexander.local>
2017-11-24 16:59:17 +0300

    init

---
+++ Assets/Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+
+    public Transform lightTransform;
+    public Transform objectTransform;
+	public int speed = 200;
+
+    void Update()
+    {
+        lightTransform.RotateAround(objectTransform.position, Vector3.up, speed * Time.deltaTime);
+    }
+
+}
